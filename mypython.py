0e8002e400b0eb328ea8b1303fa397986fd6d053
Devang Kale <devangkale3@gmail.com>
2023-03-25 21:57:16 -0400

    calculator function

--- mypython.py
+++ mypython.py
@@ -1,1 +1,21 @@
+def addNums(a, b):
+    return a + b
 
+
+def multiplyNums(a, b):
+    return a * b
+
+
+def subtractNums(a, b):
+    return a - b
+
+
+def divideNums(a, b):
+    return a / b
+
+
+def main():
+    print(addNums(5, 4))
+    print(multiplyNums(5, 4))
+    print(subtractNums(5, 4))
+    print(divideNums(5, 4))
