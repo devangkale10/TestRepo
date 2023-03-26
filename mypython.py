66232fbd1f296a0cec6fcd9241f3a5f8b761d0b1
Devang Kale <devangkale3@gmail.com>
2023-03-25 22:02:30 -0400

    added extended calc functionality

--- mypython.py
+++ mypython.py
@@ -14,8 +14,28 @@ def divideNums(a, b):
     return a / b
 
 
+def remainderNums(a, b):
+    return a % b
+
+
+def negateNums(a):
+    return -a
+
+
+def squareNums(a):
+    return a * a
+
+
+def cubeNums(a):
+    return a * a * a
+
+
 def main():
     print(addNums(5, 4))
     print(multiplyNums(5, 4))
     print(subtractNums(5, 4))
     print(divideNums(5, 4))
+    print(remainderNums(5, 4))
+    print(negateNums(5))
+    print(squareNums(5))
+    print(cubeNums(5))
