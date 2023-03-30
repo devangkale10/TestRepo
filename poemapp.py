ad40a6c150b0b557731a5095d0191db50a63d9ca
Devang Kale <devangkale3@gmail.com>
2023-03-29 20:39:34 -0400

    Poem App

---
+++ poemapp.py
@@ -0,0 +1,8 @@
+"""This is a python script that generates a poem based on the user's input."""
+
+
+def main():
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    adjective = input("Enter an adjective: ")
+    print("The " + noun + " " + verb + " the " + adjective + " dog.")
